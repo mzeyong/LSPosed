5805153e2485d926425b9f6dc81e032cc1333c79
k2yk <mzeyong@gmail.com>
2024-10-03 06:49:39 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,10 +66,10 @@ val injectedPackageUid by extra(2000)
 val defaultManagerPackageName by extra("org.lsposed.manager")
 val verCode by extra(commitCount)
 val verName by extra(latestTag)
-val androidTargetSdkVersion by extra(35)
+val androidTargetSdkVersion by extra(34)
 val androidMinSdkVersion by extra(27)
 val androidBuildToolsVersion by extra("34.0.0")
-val androidCompileSdkVersion by extra(35)
+val androidCompileSdkVersion by extra(34)
 val androidCompileNdkVersion by extra("26.1.10909125")
 val androidSourceCompatibility by extra(JavaVersion.VERSION_17)
 val androidTargetCompatibility by extra(JavaVersion.VERSION_17)
@@ -104,7 +104,9 @@ subprojects {
                 abortOnError = true
                 checkReleaseBuilds = false
             }
-
+            kotlinOptions {
+                 jvmTarget=17
+            }
             compileOptions {
                 sourceCompatibility = androidSourceCompatibility
                 targetCompatibility = androidTargetCompatibility
